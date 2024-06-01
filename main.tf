624accf2e371c1e629d9061f4194032cec0df989
Ankit Kumar <159402727+Ankitadi5746@users.noreply.github.com>
2024-06-01 21:01:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 provider "aws" {
+access key ="AKIA3FLDZXR2HDXVDDVH"
+secret key = "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"
   region = "us-east-1"
 }
 
@@ -6,6 +8,6 @@ resource "aws_instance" "dev" {
     ami = "ami-00beae93a2d981137"
     instance_type = "t2.micro"
     tags = {
-      Name = "dev-ec2"
+      Name = "pipeline-ec2"
     }
 }
