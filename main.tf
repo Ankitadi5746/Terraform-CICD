4214e968013861dce46989fc7c6f0b4e2362e745
Ankit Kumar <159402727+Ankitadi5746@users.noreply.github.com>
2024-06-01 23:02:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ secret_key = "myXyEvMIrrJg+Ma9/nwnIz0R5mW4kSFnC7Fzxl1q"
   region = "us-east-1"
 }
 
-resource "aws_instance" "DAAL" {
+resource "aws_instance" "DAbahgt" {
     ami = "ami-00beae93a2d981137"
     instance_type = "t2.micro"
     tags = {
